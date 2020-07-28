1aea9c22b3f1cbb72a0bc67cf18c6ed5c7f3bdef
ianfletcher55 <ian.fletcher55@gmail.com>
2020-07-28 13:29:24 -0500

    adds show page with initial necessary data and *hardcoded* wiki links for quests

--- src/views/BoardShow.vue
+++ src/views/BoardShow.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="board-show">
-    <h1>{{ message }}</h1>
+    <h1>{{ board.name }}</h1>
+    <div v-for="list in board.lists">
+      <h2>{{ list.name }}</h2>
+      <div v-for="quest in list.quests">
+        {{ quest.name }} <br>
+        <button v-on:click="openLink()">Wiki Link</button>
+      </div>
+      <div v-for="item in list.items">
+        {{ item.name }}
+      </div>
+      <div v-for="note in list.notes">
+        {{ note.text }}
+      </div>
+    </div>
+
+
+
   </div>
 </template>
 
@@ -8,13 +24,24 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "This is the board show"
+      board: {},
+      linkName: "Rag_and_Bone_Man"
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    axios.get(`/api/boards/${this.$route.params.id}`).then(response => {
+      console.log(response.data);
+      this.board = response.data;
+    });
+  },
+  methods: {
+    openLink: function() {
+      window.open(`https://oldschool.runescape.wiki/w/${this.linkName}`);
+    }
+  }
 };
 </script>
